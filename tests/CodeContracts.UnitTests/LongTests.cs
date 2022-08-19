d091a4b8f4d8422605bb4bf1ebf19f81656163d9
lr <lrmail>
2022-08-19 14:53:58 +0200

    Add LongTests

---
+++ tests/CodeContracts.UnitTests/LongTests.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeContracts.UnitTests;
+
+public class LongTests : ComparableTests<long>
+{
+    protected override IEnumerable<long> PositiveValues { get; } = new List<long>
+    {
+        0,
+        1,
+        long.MaxValue
+    };
+
+    protected override IEnumerable<long> NegativeValues { get; } = new List<long>
+    {
+        -1,
+        long.MinValue
+    };
+}
