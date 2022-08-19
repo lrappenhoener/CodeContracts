baf4c1321e6d5d189305d9c98219c500d33a1375
lr <lrmail>
2022-08-19 14:55:05 +0200

    Add DecimalTests

---
+++ tests/CodeContracts.UnitTests/DecimalTests.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeContracts.UnitTests;
+
+public class DecimalTests : ComparableTests<Decimal>
+{
+    protected override IEnumerable<Decimal> PositiveValues { get; } = new List<Decimal>
+    {
+        0,
+        1,
+        Decimal.MaxValue
+    };
+
+    protected override IEnumerable<Decimal> NegativeValues { get; } = new List<Decimal>
+    {
+        -1,
+        Decimal.MinValue
+    };
+}
