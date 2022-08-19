6eb3046ac91145a5e5ec1fe0f4b29cfc146e0e6b
lr <lrmail>
2022-08-19 13:07:39 +0200

    Add test Positive_Requirement_Successful_Asserts_Int

---
+++ tests/CodeContracts.UnitTests/NumberTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CodeContracts.UnitTests
+{
+    public class NumberTests
+    {
+        [Theory]
+        [InlineData(int.MaxValue, false)]
+        [InlineData(1, false)]
+        [InlineData(0, false)]
+        [InlineData(-1, true)]
+        [InlineData(int.MinValue, true)]
+        public void Positive_Requirement_Successful_Asserts_Int(int number, bool throws)
+        {
+            var exception = Record.Exception(() =>
+                Contract.For(number).Positive().Ok());
+
+            (exception != null).Should().Be(throws);
+        }
+    }
+}
