875d9f39eb36e137775ab029de7d4978229e2217
lr <lrmail>
2022-08-19 18:28:08 +0200

    Add test InRange_Requirement_Successful_Asserts_InvalidRanges

---
+++ tests/CodeContracts.UnitTests/ComparableTests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+
+namespace CodeContracts.UnitTests;
+
+public abstract class ComparableTests<T> where T : IComparable
+{
+    protected abstract IEnumerable<T> PositiveValues { get; }
+    protected abstract IEnumerable<T> NegativeValues { get; }
+    protected abstract IEnumerable<Range> RangesThatAreValid { get; }
+    protected abstract IEnumerable<Range> RangesThatAreInvalid { get; }
+
+    [Fact]
+    public void Positive_Requirement_Successful_Asserts_Positive_Values()
+    {
+        foreach (var positiveValue in PositiveValues)
+        {
+            var exception = Record.Exception(() =>
+                Contract.For(positiveValue).Positive().Ok());
+
+            exception.Should().BeNull();
+        }
+    }
+
+    [Fact]
+    public void Positive_Requirement_Successful_Asserts_Negative_Values()
+    {
+        foreach (var positiveValue in PositiveValues)
+        {
+            var exception = Record.Exception(() =>
+                Contract.For(positiveValue).Positive().Ok());
+
+            exception.Should().BeNull();
+        }
+    }
+
+    [Fact]
+    public void Negative_Requirement_Successful_Asserts_Negative_Values()
+    {
+        foreach (var negativeValue in NegativeValues)
+        {
+            var exception = Record.Exception(() =>
+                Contract.For(negativeValue).Negative().Ok());
+
+            exception.Should().BeNull();
+        }
+    }
+
+    [Fact]
+    public void Negative_Requirement_Successful_Asserts_Positive_Values()
+    {
+        foreach (var negativeValue in NegativeValues)
+        {
+            var exception = Record.Exception(() =>
+                Contract.For(negativeValue).Negative().Ok());
+
+            exception.Should().BeNull();
+        }
+    }
+
+    [Fact]
+    public void InRange_Requirement_Successful_Asserts_ValidRanges()
+    {
+        foreach (var range in RangesThatAreValid)
+        {
+            var exception = Record.Exception(() => Contract.For(range.Value).InRange(range.Min, range.Max).Ok());
+            exception.Should().BeNull();
+        }
+    }
+
+    [Fact]
+    public void InRange_Requirement_Successful_Asserts_InvalidRanges()
+    {
+        foreach (var range in RangesThatAreInvalid)
+        {
+            var exception = Record.Exception(() => Contract.For(range.Value).InRange(range.Min, range.Max).Ok());
+            exception.Should().NotBeNull();
+        }
+    }
+}
