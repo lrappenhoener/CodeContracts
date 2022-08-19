40c04feb8c5eae5b909ee39ad224643c20f2dac3
lr <lrmail>
2022-08-19 15:35:55 +0200

    Add FloatTests

---
+++ tests/CodeContracts.UnitTests/FloatTests.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeContracts.UnitTests;
+
+public class FloatTests : ComparableTests<float>
+{
+    protected override IEnumerable<float> PositiveValues { get; } = new List<float>
+    {
+        0.0f,
+        1.0f,
+        float.MaxValue
+    };
+
+    protected override IEnumerable<float> NegativeValues { get; } = new List<float>
+    {
+        -1.0f,
+        float.MinValue
+    };
+}
