f390b7f441a61667d078a1f4a5afe6ce7703a31b
lr <lrmail>
2022-08-19 13:11:58 +0200

    Introduce ContractDoubleConditions

---
+++ src/CodeContracts/ContractDoubleConditions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace CodeContracts;
+
+public class ContractDoubleConditions
+{
+    private readonly double _target;
+    private Expression<Func<double, bool>>? _condition;
+
+    public ContractDoubleConditions(double target)
+    {
+        _target = target;
+    }
+
+    public ContractDoubleConditions Positive()
+    {
+        UpdateConditions(i => i >= 0);
+        return this;
+    }
+
+    public void Ok()
+    {
+        if (_condition == null) return;
+        var compiled = _condition.Compile();
+        Debug.Assert(compiled.Invoke(_target));
+    }
+
+    private void UpdateConditions(Expression<Func<double, bool>> condition)
+    {
+        _condition = _condition == null ? condition : CreateAndExpression(_condition, condition);
+    }
+
+    private Expression<Func<double, bool>> CreateAndExpression(Expression<Func<double, bool>> first, Expression<Func<double, bool>> second)
+    {
+        var firstCondition = first.Compile();
+        var secondCondition = second.Compile();
+        return o => firstCondition(o) && secondCondition(o);
+    }
+}
