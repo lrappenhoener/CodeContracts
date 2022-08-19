f453581ef3ba9d9327372e3e33da5bfd7fa398b5
lr <lrmail>
2022-08-19 14:56:17 +0200

    Add ShortTests

---
+++ tests/CodeContracts.UnitTests/ShortTests.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeContracts.UnitTests;
+
+public class ShortTests : ComparableTests<short>
+{
+    protected override IEnumerable<short> PositiveValues { get; } = new List<short>
+    {
+        0,
+        1,
+        short.MaxValue
+    };
+
+    protected override IEnumerable<short> NegativeValues { get; } = new List<short>
+    {
+        -1,
+        short.MinValue
+    };
+}
