e68761bdaf1317bcc74a78023d3624bd3c750a47
lr <lrmail>
2022-08-20 10:27:36 +0200

    Add test NotNull_Requirement_When_Null_Throws

---
+++ tests/CodeContracts.UnitTests/EnumerableTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CodeContracts.UnitTests
+{
+    public class EnumerableTests
+    {
+        [Fact]
+        public void NotNull_Requirement_When_Null_Throws()
+        {
+            IEnumerable enumerable = null;
+
+            var exception = Record.Exception(() => Contract.For(enumerable).NotNull().Ok());
+
+            exception.Should().NotBeNull();
+        }
+    }
+}
