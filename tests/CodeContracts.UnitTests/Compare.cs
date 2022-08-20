f0bf3213496cc6718c84385c7a42dd951810a041
lr <lrmail>
2022-08-20 08:27:19 +0200

    Introduce Compare

---
+++ tests/CodeContracts.UnitTests/Compare.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeContracts.UnitTests;
+
+public class Compare
+{
+    public Compare(IComparable value, IComparable max)
+    {
+        Value = value;
+        Max = max;
+    }
+
+    public IComparable Value { get; }
+    public IComparable Max { get; }
+}
