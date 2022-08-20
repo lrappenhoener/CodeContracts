d28eb6cf5c429911f5f99c6e1645e1480fe80c2e
lr <lrmail>
2022-08-20 13:44:32 +0200

    Refactor ContractEnumerableConditions

--- src/CodeContracts/ContractEnumerableConditions.cs
+++ src/CodeContracts/ContractEnumerableConditions.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections;
-using System.Diagnostics;
-using System.Linq.Expressions;
+﻿using System.Collections;
 
 namespace PCC.Libraries.CodeContracts;
 
-public class ContractEnumerableConditions
+public sealed class ContractEnumerableConditions : BaseConditions<IEnumerable?>
 {
-    private readonly IEnumerable? _target;
-    private Expression<Func<IEnumerable?, bool>>? _condition;
-
     public ContractEnumerableConditions(IEnumerable? target)
     {
-        _target = target;
+        Target = target;
     }
+
+    protected override IEnumerable? Target { get; }
 
     public ContractEnumerableConditions NotNull()
     {
@@ -31,25 +27,5 @@ public class ContractEnumerableConditions
     {
         var enumerator = enumerable.GetEnumerator();
         return enumerator.MoveNext();
-    }
-
-    private void UpdateConditions(Expression<Func<IEnumerable?, bool>> condition)
-    {
-        _condition = _condition == null ? condition : CreateAndExpression(_condition, condition);
-    }
-
-    private Expression<Func<IEnumerable?, bool>> CreateAndExpression(Expression<Func<IEnumerable?, bool>> first,
-        Expression<Func<IEnumerable?, bool>> second)
-    {
-        var firstCondition = first.Compile();
-        var secondCondition = second.Compile();
-        return o => firstCondition(o) && secondCondition(o);
-    }
-
-    public void Ok()
-    {
-        if (_condition == null) return;
-        var compiled = _condition.Compile();
-        Debug.Assert(compiled.Invoke(_target));
     }
 }
