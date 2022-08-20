6349ee9c03c837e4b772f0f3bcbaf06c1573873d
lr <lrmail>
2022-08-20 09:25:09 +0200

    Add test NotNull_Requirement_Successful_Asserts

---
+++ tests/CodeContracts.UnitTests/StringTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CodeContracts.UnitTests
+{
+    public class StringTests
+    {
+        [Theory]
+        [InlineData("Foo", false)]
+        [InlineData(null, true)]
+        public void NotNull_Requirement_Successful_Asserts(string value, bool throws)
+        {
+            var exception = Record.Exception(() => Contract.For(value).NotNull().Ok());
+
+            var hasThrown = exception != null;
+            hasThrown.Should().Be(throws);
+        }
+    }
+}
