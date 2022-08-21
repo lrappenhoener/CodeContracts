6ccdc8799f7736015b0b04dda9562ba693afb220
lr <lrmail>
2022-08-21 16:19:59 +0200

    Add test Requires_Successful_Validates_Valid_Requirements

---
+++ tests/CodeContracts.UnitTests/ContractTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace CodeContracts.UnitTests
+{
+    public class ContractTests
+    {
+        [Fact]
+        public void Requires_Successful_Validates_Valid_Requirements()
+        {
+            var requirements = CreateValidRequirements();
+            
+            var exception = Record.Exception(() => Contract.Requires(requirements));
+
+            exception.Should().BeNull();
+        }
+
+        private ContractRequirements<double> CreateValidRequirements()
+        {
+            var target = 50.0d;
+
+            return Requirements
+                .For(target)
+                .Lesser(100.0d)
+                .Greater(25.0d)
+                .InRange(45.0d, 55.0d)
+                .GreaterEquals(target)
+                .LesserEquals(target)
+                .Positive();
+        }
+    }
+}
