6c28d4795dba37366cd7dd596d36e57de45f2bde
lr <lrmail>
2022-08-21 16:19:41 +0200

    Introduce Contract

---
+++ src/CodeContracts/Contract.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace CodeContracts
+{
+    public static class Contract
+    {
+        [Conditional("DEBUG")]
+        public static void Requires<T>(ContractRequirements<T> requirements, string message = "")
+        {
+            if (requirements.Ok()) return;
+            Debug.WriteLine(false, message);
+            throw new CodeContractException(message);
+        }
+    }
+}
