635f105dc421aa4aa3584619f848cfb6b6b9df0c
lr <lrmail>
2022-08-21 19:06:00 +0200

    Add Requires(Func<bool> predicate)

--- src/CodeContracts/Contract.cs
+++ src/CodeContracts/Contract.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 
 namespace CodeContracts
 {
@@ -8,6 +9,14 @@ namespace CodeContracts
         public static void Requires<T>(ContractRequirements<T> requirements, string message = "")
         {
             if (requirements.Ok()) return;
+            Debug.WriteLine(false, message);
+            throw new CodeContractException(message);
+        }
+
+        [Conditional("DEBUG")]
+        public static void Requires(Func<bool> predicate, string message = "")
+        {
+            if (predicate()) return;
             Debug.WriteLine(false, message);
             throw new CodeContractException(message);
         }
